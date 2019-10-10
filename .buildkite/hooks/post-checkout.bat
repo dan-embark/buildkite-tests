0d6956820f5ef09aeec2b175d5536cc3604d2a56
Dan Engelbrecht <dan.engelbrecht>
2019-10-10 16:47:11 +0200

    Corrent naming for hook

--- .buildkite/hooks/post-checkout.bat
+++
@@ -1,4 +0,0 @@
-set -eu
-echo '--- Setting git clean flags, might be to late'
-
-set BUILDKITE_GIT_CLEAN_FLAGS = -fd
--- .buildkite/hooks/post-checkout.sh
+++
@@ -1,4 +0,0 @@
-set -eu
-echo '--- Setting git clean flags, might be to late'
-
-export BUILDKITE_GIT_CLEAN_FLAGS="-fd"
